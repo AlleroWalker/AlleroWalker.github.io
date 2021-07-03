f4c1fdf472082e19a06c7c2d159a5a7b78b705ea
AlleroWalker <52800353+AlleroWalker@users.noreply.github.com>
2021-07-04 01:28:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        adaptiveHeight:true,
+        slidesToShow:1,
+        speed: 1000,
+        autoplay:true,
+        autoplaySpeed: 1000,
+
+    });
+});
