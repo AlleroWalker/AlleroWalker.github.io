80cfdd36ebc65f6025e847bf3c4fa337e0581b90
AlleroWalker <52800353+AlleroWalker@users.noreply.github.com>
2021-07-19 17:40:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,9 +16,3 @@ $(document).ready(function(){
         dots: true,
     });
 });
-
-$('.menu_btn').on('click', function(e){
-    e.preventDefault;
-    $(this).toggleClass('menu_btn_active');
-    $('.menu_nav').toggleClass('menu_nav_active');
-});
