8fa78470456e075a68150a47f9cd40ef5f7ba815
AlleroWalker <52800353+AlleroWalker@users.noreply.github.com>
2021-07-19 23:32:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $('.menu_btn').on('click', function(e){
     e.preventDefault;
-    $(this). toggleClass('menu_btn_active');
+    $(this).toggleClass('menu_btn_active');
     $('.menu_nav').toggleClass('menu_nav_active');
 })
