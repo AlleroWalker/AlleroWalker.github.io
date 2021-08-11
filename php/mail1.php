30b59ea142c6bd5987b9b439f984fc2e99cb058d
Allero Walker <AlleroWalker@gmail.com>
2021-08-11 11:13:53 +0300

    Normal
    !

---
+++ php/mail1.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST ['client_name_'];
+$email = $_POST ['client_email_'];
+$massage1 = $_POST ['massage_'];
+//$name . " <br>" . $email . " <br>" . $massage1 .
+
+//$phone = $_POST ['phone_'];
+//$massage2 = $_POST ['massage2'];
+//$radio = $_POST ['modal_usage_'];
+//$phone . " <br>" . $radio . " <br>" . $massage2
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';                                                                                              // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'KALDUNSILA@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'LEGALAS15011996alex'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('KALDUNSILA@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('AlleroWalker@gmail.com');     // Кому будет уходить письмо
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Тема письма';
+$mail->Body    =  '' . $name . " <br>" . $email . " <br>" . $massage1;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+?>
